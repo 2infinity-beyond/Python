2b01d1a40024a46f3f3326ff8ab2ee58d5bec39d
2infinity-beyond <rstovall@students.mchenry.edu>
2025-01-20 13:13:43 -0600

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+'''
+Hello World
+Name: Regina Stovall
+github link: https://github.com/2infinity-beyond/Python/upload/main
+'''
+
+def main():
+    # your code here
+    print("Hello World")
+    return
+
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testfile('helloworld_doctest.py'))
