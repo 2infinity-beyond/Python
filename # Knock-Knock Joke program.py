661d598463e1a5922325e93bc009fd2ef8b3b7a5
2infinity-beyond <rstovall@students.mchenry.edu>
2025-01-26 15:03:43 -0600

    print_statement

---
+++ # Knock-Knock Joke program.py
@@ -0,0 +1,21 @@
+# Knock-Knock Joke program
+'''Name: Regina Stovall
+github link: https://github.com/2infinity-beyond/Python'''
+
+# the following is a "knock-knock joke" excerpt from a short story created by yours truly.
+# combining multiple lines by using escape code  
+print("...then, I suddenly hear a loud", "\nKnock, KNOCK!!! \nSo I cautiously approach the door. \nThen I nervously call out, \n\"Wh- who's there??\"")
+#using end keyword in combination with escape code
+print("Then came a quick reply from the voice of a woman", end="... \n")
+#using more escape characters
+print("\"Juicy!\"")
+print("Now I don't recognize her voice so I inquistively ask, \n\"Juicy, WHO??\" (I literally have no clue who she could be).")
+print("Next, she suddenly snaps back saying,")
+# introducing sep keyword
+print("\"JUI", "CY", sep="-", end=" ") 
+print("ME THROUGH THE PEEPHOLE OR NOT?!!\"")
+# using an empty argument to create a new line
+print() 
+print("I","AM","STUNNED.", sep=" - ") 
+#using escape code to create a new line at the beginning of a string
+print("\nA part of me wants to respond but I'm too perplexed because why didn't I think of that first? \n")
