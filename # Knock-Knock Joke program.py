6bde67972fdefcc72b78b301f8110f0b1bcc946a
2infinity-beyond <rstovall@students.mchenry.edu>
2025-01-26 14:50:03 -0600

    Add files via upload

---
+++ # Knock-Knock Joke program.py
@@ -0,0 +1,16 @@
+# Knock-Knock Joke program
+'''Name: Regina Stovall
+github link: https://github.com/2infinity-beyond/Python'''
+
+# the following is a "knock-knock joke" excerpt from a short story created by yours truly.
+
+print("...then, I suddenly hear a loud", "\nKnock, KNOCK!!! \nSo I cautiously approach the door. \nThen I nervously call out, \n\"Wh- who's there??\"")
+print("Then came a quick reply from the voice of a woman", end="... ")
+print("\"Juicy!\"")
+print("Now I don't recognize her voice so I inquistively ask, \n\"Juicy, WHO??\" (I literally have no clue who she could be).")
+print("Next, she suddenly snaps back saying,")
+print("\"JUI", "CY", sep="-", end=" ") 
+print("ME THROUGH THE PEEPHOLE OR NOT?!!\"")
+print()
+print("I","AM","STUNNED.", sep=" - ") 
+print("\nA part of me wants to respond but I'm too perplexed because why didn't I think of that first? \n")
