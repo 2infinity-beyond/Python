91a53da87ad73a0f32d6b1d86a8d22bdca40210d
2infinity-beyond <rstovall@students.mchenry.edu>
2025-03-10 18:24:04 -0500

    Add files via upload

---
+++ Modules/Main.py
@@ -0,0 +1,7 @@
+from math_operations import Calculator
+
+result = Calculator.add(5,3)
+print("Addition result:", result)
+
+result = Calculator.subtract(10, 4)
+print("Subtraction result:", result)
---
+++ Modules/math_operations/geometry.py
@@ -0,0 +1,9 @@
+def rec_area(l, w):
+    return l * w
+
+def tri_area(a, b):
+    return 0.5 * a * b
+
+def circle_area(r, pi = 3.14159):
+    return (r**2) * pi
+
